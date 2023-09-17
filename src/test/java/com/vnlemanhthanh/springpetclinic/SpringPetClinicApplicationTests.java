e90f1dcb889734fd9bc70d4728f03694078349cc
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-17 16:22:53 +0700

    downgrade to jdk 8

--- src/test/java/com/vnlemanhthanh/springpetclinic/SpringPetClinicApplicationTests.java
+++ src/test/java/com/vnlemanhthanh/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,6 +1,6 @@
 package com.vnlemanhthanh.springpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
