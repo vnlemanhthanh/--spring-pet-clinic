ac96254df928d33876755fb55da35630a117814a
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-17 10:27:03 +0700

    adding POJOs. Closes #2.

---
+++ src/main/java/com/vnlemanhthanh/springpetclinic/model/Person.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.springpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
