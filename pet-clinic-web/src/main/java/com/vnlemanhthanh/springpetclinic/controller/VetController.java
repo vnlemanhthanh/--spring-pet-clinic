f66cb596ed8abc36e58950e145f84cb84be97be0
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-19 23:55:40 +0700

    Added Vets List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/com/vnlemanhthanh/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/vnlemanhthanh/springpetclinic/controller/VetController.java
@@ -4,14 +4,23 @@
 
 package com.vnlemanhthanh.springpetclinic.controller;
 
+import com.vnlemanhthanh.springpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
