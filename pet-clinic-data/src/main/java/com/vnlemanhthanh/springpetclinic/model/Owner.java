5c6238ee29f3f37df8f7959b621173c6e7c5ac78
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-20 23:25:34 +0700

    added visits, updated owner with pet relationship. closes #26

--- pet-clinic-data/src/main/java/com/vnlemanhthanh/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/vnlemanhthanh/springpetclinic/model/Owner.java
@@ -4,5 +4,17 @@
 
 package com.vnlemanhthanh.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
