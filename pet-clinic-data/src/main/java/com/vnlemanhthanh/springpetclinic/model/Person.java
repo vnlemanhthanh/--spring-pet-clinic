63346dbf1c51dc7ce453e48784dd8ec2968d0f7a
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-29 15:00:30 +0700

    updated owner with JPA entity mappings. closes #36

--- pet-clinic-data/src/main/java/com/vnlemanhthanh/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/vnlemanhthanh/springpetclinic/model/Person.java
@@ -4,8 +4,16 @@
 
 package com.vnlemanhthanh.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/com/vnlemanhthanh/springpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vnlemanhthanh/springpetclinic/model/Pet.java
@@ -4,13 +4,25 @@
 
 package com.vnlemanhthanh.springpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
